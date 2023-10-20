44d3e444957a559ab3b38fec907e6ce851f39909
LeverActionGT <63264121+LeverActionGT@users.noreply.github.com>
2023-10-20 17:10:25 -0400

    stl update, render update, js reformat

--- script.js
+++ script.js
@@ -1,101 +1,120 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const darkModeToggle = document.getElementById('darkModeToggle');
-  if (!darkModeToggle) {
-      console.error('Dark mode button not found!');
-      return;
-  }
+document.addEventListener("DOMContentLoaded", (event) => {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    if (!darkModeToggle) {
+        console.error("Dark mode button not found!");
+        return;
+    }
 
-  const currentTheme = localStorage.getItem('theme');
-  console.log('Current Theme:', currentTheme);
+    const currentTheme = localStorage.getItem("theme");
+    console.log("Current Theme:", currentTheme);
 
-  if (currentTheme === 'dark') {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      darkModeToggle.classList.add('dark');
-  }
+    if (currentTheme === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark");
+        darkModeToggle.classList.add("dark");
+    }
 
-  darkModeToggle.addEventListener('click', function() {
-      const current = document.documentElement.getAttribute('data-theme');
-      console.log('Current data-theme:', current);
-      
-      let theme = (current === 'dark') ? 'light' : 'dark';
-      document.documentElement.setAttribute('data-theme', theme);
-      localStorage.setItem('theme', theme);
-      darkModeToggle.classList.toggle('dark');
-  });
+    darkModeToggle.addEventListener("click", function () {
+        const current = document.documentElement.getAttribute("data-theme");
+        console.log("Current data-theme:", current);
+
+        let theme = current === "dark" ? "light" : "dark";
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+        darkModeToggle.classList.toggle("dark");
+    });
 });
 
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
     const carousel1 = new Siema({
-        selector: '#carousel1'
+        selector: "#carousel1",
     });
 
     const carousel2 = new Siema({
-        selector: '#carousel2'
+        selector: "#carousel2",
     });
 
-    document.querySelectorAll('.prev').forEach(button => {
-        button.addEventListener('click', () => {
-            const target = button.getAttribute('data-target');
-            if (target === 'carousel1') {
+    document.querySelectorAll(".prev").forEach((button) => {
+        button.addEventListener("click", () => {
+            const target = button.getAttribute("data-target");
+            if (target === "carousel1") {
                 carousel1.prev();
-            } else if (target === 'carousel2') {
+            } else if (target === "carousel2") {
                 carousel2.prev();
             }
         });
     });
 
-    document.querySelectorAll('.next').forEach(button => {
-        button.addEventListener('click', () => {
-            const target = button.getAttribute('data-target');
-            if (target === 'carousel1') {
+    document.querySelectorAll(".next").forEach((button) => {
+        button.addEventListener("click", () => {
+            const target = button.getAttribute("data-target");
+            if (target === "carousel1") {
                 carousel1.next();
-            } else if (target === 'carousel2') {
+            } else if (target === "carousel2") {
                 carousel2.next();
             }
         });
     });
 
-                var stl_viewer=new StlViewer
-            (
-                document.getElementById("stl_toroid"),
+    var stl_viewer = new StlViewer(
+        document.getElementById("stl_6_blade_even"),
+        {
+            auto_rotate: false,
+            zoom: "165",
+            models: [
                 {
-                  auto_rotate: true,
-                  zoom: '165',
-                  models:
-                  [
-                      {filename:"3loop.stl"},
-                  ]
-                }
-                
-            );
+                    filename: "6_blade_even.stl",
+                    color: "#3d557a",
+                    animation: {
+                        delta: {
+                            rotationz: 1,
+                            msec: 500,
+                            loop: true,
+                        },
+                    },
+                },
+            ],
+        }
+    );
 
-            var stl_viewer2 =new StlViewer
-            (
-                document.getElementById("stl_uneven"),
+    var stl_viewer2 = new StlViewer(
+        document.getElementById("stl_3_loop_toroidal"),
+        {
+            auto_rotate: false,
+            zoom: "165",
+            models: [
                 {
-                  auto_rotate: true,
-                  zoom: '165',
-                  models:
-                  [
-                      {filename:"uneven.stl"},
-                  ]
-                }
-                
-            );
+                    filename: "3_loop_toroidal.stl",
+                    color: "#3d557a",
+                    animation: {
+                        delta: {
+                            rotationz: 1,
+                            msec: 500,
+                            loop: true,
+                        },
+                    },
+                },
+            ],
+        }
+    );
 
-            var stl_viewer3 =new StlViewer
-            (
-                document.getElementById("stl_6blade"),
+    var stl_viewer3 = new StlViewer(
+        document.getElementById("stl_6_blade_uneven"),
+        {
+            auto_rotate: false,
+            zoom: "165",
+            models: [
                 {
-                  auto_rotate: true,
-                  zoom: '165',
-                  models:
-                  [
-                      {filename:"6-blade.stl"},
-                  ]
-                }
-                
-            );
+                    filename: "6_blade_uneven.stl",
+                    color: "#3d557a",
+                    animation: {
+                        delta: {
+                            rotationz: 1,
+                            msec: 500,
+                            loop: true,
+                        },
+                    },
+                },
+            ],
+        }
+    );
 });
