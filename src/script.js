700affc997de8a3a3414157f357279522e6a69ca
nv <nkvijay08@gmail.com>
2023-10-20 00:02:34 -0400

    updated website

---
+++ src/script.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  if (!darkModeToggle) {
+      console.error('Dark mode button not found!');
+      return;
+  }
+
+  const currentTheme = localStorage.getItem('theme');
+  console.log('Current Theme:', currentTheme);
+
+  if (currentTheme === 'dark') {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      darkModeToggle.classList.add('dark');
+  }
+
+  darkModeToggle.addEventListener('click', function() {
+      const current = document.documentElement.getAttribute('data-theme');
+      console.log('Current data-theme:', current);
+      
+      let theme = (current === 'dark') ? 'light' : 'dark';
+      document.documentElement.setAttribute('data-theme', theme);
+      localStorage.setItem('theme', theme);
+      darkModeToggle.classList.toggle('dark');
+  });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const carousel1 = new Siema({
+        selector: '#carousel1'
+    });
+
+    const carousel2 = new Siema({
+        selector: '#carousel2'
+    });
+
+    document.querySelectorAll('.prev').forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target');
+            if (target === 'carousel1') {
+                carousel1.prev();
+            } else if (target === 'carousel2') {
+                carousel2.prev();
+            }
+        });
+    });
+
+    document.querySelectorAll('.next').forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target');
+            if (target === 'carousel1') {
+                carousel1.next();
+            } else if (target === 'carousel2') {
+                carousel2.next();
+            }
+        });
+    });
+});
